74e830274d5a230f64c8ca030bdea0f6549e6743
alexfevrier <alex.fevrier@gmail.com>
2024-07-24 14:47:10 +0000

    welcome text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Welcome to AFE DATA APP")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For help and inspiration, head over to afedata.com"
 )
